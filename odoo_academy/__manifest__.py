0ac0703d68fbaf8e66102afb0623c85d0d48d87e
Angelica Meza <mezaangelica657@gmail.com>
2022-03-08 21:53:58 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Academy",
+    
+    'summary': """ Informacion de la academia de Odoo para fines informativos""",
+    
+    'description': """"
+        Modulo para fines de la capacitacion
+        -Quiero ver como se representa esto en la insterfaz de Odoo
+        -Ya que esto no lo habia hecho
+        -Siempre puedo agregar cosas aca cuando actualizo el Modulo.
+    """,
+    
+    
+    'author': "Angelica Meza",
+    
+    'website': 'https://www.itsalescorp.com/',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+
+    ],
+    
+    'demo': [
+
+    ],
+}
